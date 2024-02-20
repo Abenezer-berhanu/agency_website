c337e76aa88b6a52ace999e1f3dac0a6b0394893
Abenezer <abenezerberhanu40@gmail.com>
2024-02-20 13:53:07 +0300

    add responsivness to signle portfolio page

--- src/app/portfolios/[id]/page.tsx
+++ src/app/portfolios/[id]/page.tsx
@@ -45,7 +45,7 @@ async function page({ params }: { params: { id: string } }) {
           <h1 className="font-bold text-2xl mt-3">{data.title}</h1>
           <small>{data.smallDesc}</small>
           <small className="my-2">{data.date}</small>
-          <div className="w-full relative h-[400px] mb-20">
+          <div className="w-full relative h-[400px] sm:mb-20">
             <Image
               src={data.thumbnail.url}
               alt={data.title}
@@ -54,22 +54,25 @@ async function page({ params }: { params: { id: string } }) {
               quality={100}
               className="w-full h-full object-cover rounded-md"
             />
-            <div className="w-full absolute bottom-[-40px] flex justify-between p-2">
-              <span className="w-fit sm:px-8 px-4 py-3 bg-black text-white flex flex-col gap-1 rounded-md capitalize text-sm">
+            <div className="w-full absolute top-0 sm:bottom-[-40px] flex max-sm:flex-col max-sm:h-full justify-around sm:justify-between p-2">
+              <span className="w-fit sm:px-8 px-2 sm:py-3 py-1 bg-black text-white flex flex-col rounded-md capitalize text-sm">
                 <b>Category:</b> <small>{data.category}</small>
               </span>
-              <span className="w-fit sm:px-8 px-4 py-3 bg-black text-white flex flex-col gap-1 rounded-md capitalize text-sm">
-                <b>Category:</b> <small>{data.category}</small>
+              <span className="w-fit sm:px-8 px-2 sm:py-3 py-1 bg-black text-white flex flex-col rounded-md capitalize text-sm">
+                <b>Client:</b> <small>{data.client}</small>
               </span>
-              <span className="w-fit sm:px-8 px-4 py-3 bg-black text-white flex flex-col gap-1 rounded-md capitalize text-sm">
-                <b>Category:</b> <small>{data.category}</small>
+              <span className="w-fit sm:px-8 px-2 sm:py-3 py-1 bg-black text-white flex flex-col rounded-md capitalize text-sm">
+                <b>Date:</b> <small>{data.date}</small>
               </span>
-              <span className="w-fit sm:px-8 px-4 py-3 bg-black text-white flex flex-col gap-1 rounded-md capitalize text-sm">
-                <b>Category:</b> <small>{data.category}</small>
+              <span className="w-fit sm:px-8 px-2 sm:py-3 py-1 bg-black text-white flex flex-col rounded-md capitalize text-sm">
+                <b>Location:</b>{" "}
+                <small>
+                  {data.country} &nbsp; {data.city}
+                </small>
               </span>
             </div>
           </div>
-          <ol className="list-decimal">
+          <ol className="list-decimal max-sm:px-4">
             <li className="my-5 space-y-3">
               <b className="capitalize">challenge</b>
               <p className="text-sm font-serif">{data.challenge}</p>
@@ -83,7 +86,7 @@ async function page({ params }: { params: { id: string } }) {
               <p className="text-sm font-serif">{data.challenge}</p>
             </li>
           </ol>
-          <div className="w-full my-4 flex flex-wrap gap-5">
+          <div className="w-full sm:my-4 flex flex-wrap gap-5">
             {data.image.map((image: any, idx) => (
               <Dialog key={idx}>
                 <DialogTrigger>
@@ -92,7 +95,7 @@ async function page({ params }: { params: { id: string } }) {
                     alt={"image"}
                     width={500}
                     height={1000}
-                    className="max-w-[300px] h-[200px]"
+                    className="max-w-[300px] h-[200px] w-full"
                   />
                 </DialogTrigger>
                 <DialogContent>
