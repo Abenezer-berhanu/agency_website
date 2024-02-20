a949c83b3939adee802b82c9f99f7f404023f7d0
Abenezer <abenezerberhanu40@gmail.com>
2024-02-20 23:03:42 +0300

    add responsive navbar

--- src/components/UiComponents/NavBar.tsx
+++ src/components/UiComponents/NavBar.tsx
@@ -5,6 +5,17 @@ import Link from "next/link";
 import React from "react";
 import { Button } from "../ui/button";
 import { usePathname } from "next/navigation";
+import { IoMdMenu } from "react-icons/io";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
 function NavBar() {
   const path = usePathname();
   return (
@@ -33,11 +44,60 @@ function NavBar() {
           </Link>
         ))}
       </div>
-      <Link href={'/contact-us'} className="flex items-center">
-        <Button size={"sm"} className="rounded-none">
-          Get in touch
-        </Button>
-      </Link>
+      <>
+        <div className="sm:hidden">
+          <Sheet>
+            <SheetTrigger className="text-3xl h-full px-2">
+              <IoMdMenu />
+            </SheetTrigger>
+            <SheetContent>
+              <SheetHeader className="flex flex-col gap-5">
+                <SheetTitle>Abenu Company</SheetTitle>
+
+                <SheetDescription className="w-full">
+                  <Link
+                    href={"/"}
+                    className="text-black font-semibold w-full h-10 p-2"
+                  >
+                    Home
+                  </Link>
+                </SheetDescription>
+                <SheetDescription>
+                  <Link
+                    href={"/portfolios"}
+                    className="text-black font-semibold w-full h-10 p-2"
+                  >
+                    Portfolios
+                  </Link>
+                </SheetDescription>
+                <SheetDescription>
+                  <Link
+                    href={"/about"}
+                    className="text-black font-semibold w-full h-10 p-2"
+                  >
+                    About
+                  </Link>
+                </SheetDescription>
+
+                <SheetDescription>
+                  <Link
+                    href={"/contact-us"}
+                    className="text-black font-semibold w-full h-10 p-2"
+                  >
+                    Contact
+                  </Link>
+                </SheetDescription>
+              </SheetHeader>
+            </SheetContent>
+          </Sheet>
+        </div>
+
+        <Link href={"/contact-us"} className="hidden sm:flex items-center">
+          <Button size={"sm"} className="rounded-none">
+            Get in touch
+          </Button>
+        </Link>
+      </>
     </div>
   );
 }
